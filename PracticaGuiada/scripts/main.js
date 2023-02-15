acfe1afa3a54e8b167bc11e185ff17a28953d7b2
Paula Andrea Palacios <papalacios61@misena.edu.co>
2023-02-14 22:12:16 -0500

    add_profesores

--- PracticaGuiada/scripts/main.js
+++ PracticaGuiada/scripts/main.js
@@ -1,8 +1,27 @@
 import{ validateString, vAvg, checkAvg} from './validate.js';
-import{paintCard, addStudent,modalAlert} from './paint.js';
+import{paintCard, addStudent,modalAlert,addProfesores} from './paint.js';
 
 const btnAgregar = document.getElementById('btnAgregar');
 const btnMostrar = document.getElementById('btnMostrar');
+const opcion = document.getElementById('opcion');
+
+// onchange se produce cuando el valor de un elemento se ha cambiado.
+//para cambiar el contenido de texto de un elemento de texto
+
+opcion.onchange = function(){
+    const op = document.getElementById('opcion').value; 
+    if (op === 'profesor'){
+        alert("PROFESOR")
+        document.querySelector('.valor_1').innerText="Nombres y Apellidos:";
+        document.querySelector('.valor_2').innerText="Profesion:";
+        document.querySelector('.valor_3').innerText="Edad:";
+    } else if (op === 'estudiante'){
+        alert("ESTUDIANTE")
+        document.querySelector('.valor_1').innerText="Nombres:";
+        document.querySelector('.valor_2').innerText="Apellidos:";
+        document.querySelector('.valor_3').innerText="Promedio:";
+    }
+}
 
 /*Generar eventos
   1. Colocando el metodo(eventos) en el atributo onclick, onmouseover, onmouseout de la etiqueta
@@ -12,28 +31,48 @@ const btnMostrar = document.getElementById('btnMostrar');
 
 
 btnAgregar.onclick = function (){
-    //console.log('Evento mediante propiedad');
     const name = document.getElementById('nombre').value;
     const lastName = document.getElementById('apellido').value;
     const avg = parseFloat(document.getElementById('promedio').value);
-    const op = document.getElementById('opcion').value;
-    //console.log(`${name} ${lastName} ${avg} ${op}`);
-
-
-if(validateString(name) && validateString(lastName) && op != 0){
-   if((!isNaN(avg)) && (vAvg(avg))){
-    addStudent(name,lastName,avg)
-   }else{
-    document.querySelector('#promedio').value="";
-    modalAlert("Promedio Invalido");
-   }
-
-}else{
-  modalAlert("Datos invalidos, revisar los datos");
+    const nombre_p = document.getElementById('nombre').value;
+    const apellido_p = document.getElementById('apellido').value;
+    const edad_p = parseFloat(document.getElementById('promedio').value);
+    const op = document.getElementById('opcion').value; 
+    
+  
+    if (op === 'estudiante'){
+      if (validateString(name) && validateString(lastName) && op !=0){
+          if ((!isNaN(avg)) && (vAvg(avg))){
+              addStudent(name,lastName,avg)
+              document.getElementById('nombre').value="";
+              document.getElementById('apellido').value="";
+              document.querySelector('#promedio').value="";
+          } else {
+              document.querySelector('#promedio').value= "";
+              modalAlert("Promedio Invalido");
+          }
+      } else {
+          modalAlert("Datos invalidos, revisar campos");
+      }
+  } else if (op === 'profesor') {
+      if (validateString(nombre_p) && validateString(apellido_p) && op !=0){
+          if (!isNaN(edad_p)){
+              addProfesores(name,lastName,avg)
+              document.getElementById('nombre').value="";
+              document.getElementById('apellido').value="";
+              document.querySelector('#promedio').value="";
+          }
+      } else {
+          modalAlert("Datos invalidos, revisar campos");
+      }
+  }
 }
-}
-btnMostrar.addEventListener("click", function(){
-  paintCard("ESTUDIANTE");
-
-});
+btnMostrar.addEventListener('click', function(){
+  const op = document.getElementById('opcion').value;
+  if (op === 'estudiante'){
+      paintCard("ESTUDIANTE");
+  } else if (op === 'profesor') {
+      paintCard("PROFESOR");
+  }  
+})
 
--- PracticaGuiada/scripts/paint.js
+++ PracticaGuiada/scripts/paint.js
@@ -1,12 +1,14 @@
 import { checkAvg }from './validate.js';
 const cardE = document.getElementById('cardsEstudiantes');
+const cardP = document.getElementById('cardseProfesores');
+const profesores = [];
 const students = [];
 
 const paintCard = (typ) =>{
 typ = typ.toUpperCase();
 const fragment = document.createDocumentFragment();
-const templateStudent = document.querySelector
-('#templateEstudiante').content;
+const templateStudent = document.querySelector('#templateEstudiante').content;
+const templateProfesores = document.querySelector('#templateProfesor').content;
 
 if(typ === 'ESTUDIANTE'){
    for(let i of students){
@@ -20,13 +22,29 @@ if(typ === 'ESTUDIANTE'){
        fragment.appendChild(cloneTemp);
    }
 
- }else{
-
- }
+ }else if (typ === 'PROFESOR'){
+  for(let i of profesores){
+    const cloneTemp = templateProfesores.cloneNode(true);
+    cloneTemp.querySelector('.title-card').innerHTML = "Datos del <i>Profesor</i>";
+    cloneTemp.querySelector('.data-card').innerHTML = `NOMBRE: ${i.nombre_p.toUpperCase()} PROFESIÓN: ${i.apellido_p.toUpperCase()}`;
+    cloneTemp.querySelector('.text-promedio').innerHTML = `LA EDAD ES: ${i.edad_p}`;
+    fragment.appendChild(cloneTemp);
+  } 
+}
  cardE.appendChild(fragment);
+ cardP.appendChild(fragment);
 
 };
 
+const addProfesores = (name, lastName, avg)=>{
+  let profesor = {
+    nombre_p : name,
+    apellido_p : lastName,
+    edad_p: avg
+  }
+  profesores.push(profesor);
+  modalAlert('Se agrego profesor correctamente');
+}
 const addStudent = (name,lastName,avg) => {
   // Objeto literal de JS
    let student = {
@@ -35,7 +53,7 @@ const addStudent = (name,lastName,avg) => {
     prom : avg
    }
    students.push(student);
-   modalAlert('Se agrego corectamente');
+   modalAlert('Se agrego estudiante corectamente');
 };
   const modalAlert=(cad)=>{
   console.log(cad);
@@ -57,5 +75,4 @@ const addStudent = (name,lastName,avg) => {
 
 }
 
-
-export {paintCard,addStudent,modalAlert}
+export {paintCard,addStudent,modalAlert,addProfesores}
